fbe387649364b48c0eeea28f4c16ccfc7a4c91e2
Denis <zabavnikov.com@gmail.com>
2022-06-30 20:08:52 +0700

    update

--- components/modules/users/routes.ts
+++ components/modules/users/routes.ts
@@ -1,4 +1,5 @@
 export default [
   { path: '/login', name: 'users.login', file: 'login'},
+  { path: '/register', name: 'users.register', file: 'register'},
   { path: '/users/:userId(\\d+)',  name: 'users.show', file: 'show' }
 ]
