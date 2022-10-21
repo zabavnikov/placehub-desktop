749ce6fe06b2a888e90ff60c0ce9e1d50a953c29
Denis <zabavnikov.com@gmail.com>
2022-10-21 19:18:11 +0700

    update

--- components/modules/posts/components/PostFormImages.vue
+++ components/modules/posts/components/PostFormImages.vue
@@ -14,6 +14,7 @@
 </template>
 
 <script setup>
+import '../gallery.scss'
 import Draggable from 'vuedraggable'
 import { XMarkIcon } from '@heroicons/vue/24/solid'
 import { ref, computed } from 'vue'
@@ -31,10 +32,15 @@ const newSize = ref(props.modelValue)
 
 const classes = computed(() => {
   return {
-    2: 'gallery-grid-2',
-    3: 'gallery-grid-3',
-    4: 'gallery-grid-4',
-    5: 'gallery-grid-5'
+    2:  'gallery-grid-2',
+    3:  'gallery-grid-3',
+    4:  'gallery-grid-4',
+    5:  'gallery-grid-5',
+    6:  'gallery-grid-6',
+    7:  'gallery-grid-7',
+    8:  'gallery-grid-8',
+    9:  'gallery-grid-9',
+    10: 'gallery-grid-10'
   }
 })
 
@@ -60,81 +66,3 @@ const onDelete = (index) => {
   }
 }
 </script>
-
-<style lang="scss">
-.gallery {
-  @apply grid gap-px relative max-h-[360px] overflow-hidden  rounded-lg;
-
-  > div {
-    overflow: hidden;
-  }
-
-  img {
-    width: 100%;
-  }
-
-  &-grid-2 {
-    @apply grid-cols-2;
-
-    > div img {
-      height: 240px;
-    }
-  }
-
-  &-grid-3 {
-    @apply grid-rows-2 grid-flow-col;
-
-    > div:first-child {
-      @apply row-span-2;
-
-      img {
-        height: 100%;
-      }
-    }
-
-    > div:nth-child(2),
-    > div:nth-child(3), {
-      @apply col-span-1;
-
-      img {
-        height: 180px;
-      }
-    }
-  }
-
-  &-grid-4 {
-    @apply grid-cols-2;
-    max-height: none;
-
-    > div {
-      img {
-        @apply aspect-square;
-      }
-    }
-  }
-
-  &-grid-5 {
-    @apply grid-cols-6;
-    max-height: none;
-
-    > div:first-child,
-    > div:nth-child(2) {
-      @apply col-span-3;
-
-      img {
-        @apply aspect-square;
-      }
-    }
-
-    > div:nth-child(3),
-    > div:nth-child(4),
-    > div:nth-child(5), {
-      grid-column: 2 span / 2 span;
-
-      img {
-        @apply aspect-square;
-      }
-    }
-  }
-}
-</style>
--- components/modules/posts/components/PostGallery.vue
+++ components/modules/posts/components/PostGallery.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script setup>
+import '../gallery.scss'
+
 const props = defineProps({
   images: {
     type:     Array,
@@ -15,101 +17,14 @@ const props = defineProps({
 })
 
 const classes = {
-  2: 'gallery-grid-2',
-  3: 'gallery-grid-3',
-  4: 'gallery-grid-4',
-  5: 'gallery-grid-5',
-  6: 'gallery-grid-6'
+  2:  'gallery-grid-2',
+  3:  'gallery-grid-3',
+  4:  'gallery-grid-4',
+  5:  'gallery-grid-5',
+  6:  'gallery-grid-6',
+  7:  'gallery-grid-7',
+  8:  'gallery-grid-8',
+  9:  'gallery-grid-9',
+  10: 'gallery-grid-10'
 }
 </script>
-
-<style lang="scss">
-.gallery {
-  @apply grid gap-px relative max-h-[360px] overflow-hidden  rounded-lg;
-
-  > div {
-    overflow: hidden;
-  }
-
-  img {
-    width: 100%;
-  }
-
-  &-grid-2 {
-    @apply grid-cols-2;
-
-    > div img {
-      height: 240px;
-    }
-  }
-
-  &-grid-3 {
-    @apply grid-rows-2 grid-flow-col;
-
-    > div:first-child {
-      @apply row-span-2;
-
-      img {
-        height: 100%;
-      }
-    }
-
-    > div:nth-child(2),
-    > div:nth-child(3), {
-      @apply col-span-1;
-
-      img {
-        height: 180px;
-      }
-    }
-  }
-
-  &-grid-4 {
-    @apply grid-cols-2;
-    max-height: none;
-
-    > div {
-      img {
-        @apply aspect-square;
-      }
-    }
-  }
-
-  &-grid-5 {
-    @apply grid-cols-6;
-    max-height: none;
-
-    > div:first-child,
-    > div:nth-child(2) {
-      @apply col-span-3;
-
-      img {
-        @apply aspect-square;
-      }
-    }
-
-    > div:nth-child(3),
-    > div:nth-child(4),
-    > div:nth-child(5), {
-      grid-column: 2 span / 2 span;
-
-      img {
-        @apply aspect-square;
-      }
-    }
-  }
-
-  &-grid-6 {
-    @apply grid-cols-6;
-    max-height: none;
-
-    > div {
-      @apply col-span-2;
-
-      img {
-        @apply aspect-square;
-      }
-    }
-  }
-}
-</style>
